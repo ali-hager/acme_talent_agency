9a8f2366480610c7f0844bbb77dc6ad8fbbe12a6
Ali Hager <ahager2013@gmail.com>
2024-03-13 16:01:39 -0400

    data layer complete

--- server/index.js
+++ server/index.js
@@ -1,11 +1,32 @@
-const {client, createTables} = require('./db');
+const {client, createTables, createUser, fetchUsers, createSkill, fetchSkills, createUserSkill, fetchUserSkills, destroyUserSkill} = require('./db');
 
 const init = async () => {
   console.log('connecting to database');
   await client.connect();
   console.log('connected to database');
   await createTables();
-  console.log('tables created')
+  console.log('tables created');
+  const [bart, lisa, homer, mischief, spelling, burping] = await Promise.all([
+    createUser({ username: 'bart', password: '321'}),
+    createUser({ username: 'lisa', password: 'abc'}),
+    createUser({ username: 'homer', password: 'abc'}),
+    createSkill({ name: 'mischief'}),
+    createSkill({ name: 'spelling'}),
+    createSkill({ name: 'burping'})
+  ]);
+  console.log(await fetchUsers());
+  console.log(await fetchSkills());
+
+  const [bartMischief, bartSpelling] = await Promise.all([
+    createUserSkill({user_id: bart.id, skill_id: mischief.id}),
+    createUserSkill({user_id: bart.id, skill_id: spelling.id}),
+  ]);
+  console.log(await fetchUserSkills(bart.id));
+
+  // test that this errors! bart does not spell
+  // await destroyUserSkill({ user_id: homer.id, id: bartMischief.id });
+  
+  console.log(await fetchUserSkills(bart.id))
 };
 
 init();
